d1379107bef1f00b9913c356c5bc613d87027d09
Sushil Bhatta <bhattsushil57@gmail.com>
2026-01-25 21:02:55 +0545

    added files for games

--- crud_app/crud/urls.py
+++ crud_app/crud/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   path('<str:name>/',views.home,name='home'),
    path('index/',views.index,name='index'),
-   path('',views.add,name='add'),
+   path('',views.register_form,name="register"),
+   path('login/',views.login_form,name="login"),
+   path('logout/',views.logout_form,name="logout"),
+   path('home/',views.home,name="home"),
+   path('action/',views.action,name='action'),
+   path('sports/',views.sports,name='sports'),
+   path('strategy/',views.strategy,name='strategy'),
 ]
--- crud_app/crud/views.py
+++ crud_app/crud/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from .models import newtaskform
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout,authenticate
 import datetime
-def home(request,name):
-    return render(request,'crud/home.html',{"name":name.capitalize()})
+def home(request):
+    return render(request,'crud/home.html')
 
 def index(request):
   now = datetime.datetime.now()
@@ -10,12 +11,37 @@ def index(request):
   "newyear": now.month == 1 and now.day == 1
 })
   
-def add(request):
-  if request.method=='POST':
-    form = newtaskform(request.POST)
-    if form.is_valid():
-      task = form.cleaned_data['task']
-      return render(request,'crud/add.html',{'form':form,'task':task})
-  else:
-    form = newtaskform()
-  return render(request,'crud/add.html',{'form':form})
+def register_form(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'crud/register.html', {'form': form})
+
+def login_form(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST) 
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'crud/login.html', {'form': form})
+
+def logout_form(request):
+  logout(request)
+  return redirect('login')
+
+def action(request):
+  return render(request,'crud/action.html')
+
+def sports(request):
+    return render(request,'crud/sports.html')
+def strategy(request):
+    return render(request,'crud/strategy.html')
